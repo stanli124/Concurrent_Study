e9698c0e1c21b36e1462aa5ae8b2c03353a7e24d
stanli124 <1353276354@qq.com>
2022-01-13 22:19:02 +0800

    synchronized关键字实现原理

--- src/SynchronizedDemo.java
+++ src/SynchronizedDemo.java
@@ -1,13 +1,37 @@
-public class SynchronizedDemo {
+//public class SynchronizedDemo {
+//    private static int count = 0;
+//    public static void main(String[] args) {
+//
+//
+//
+//    }
+//
+//}
 
 
-
+public class SynchronizedDemo implements Runnable {
+    private static int count = 0;
     public static void main(String[] args) {
-        int i = 1;
-        i++;
-//        synchronized (SynchronizedDemo.class){
-//            System.out.println("hello synchronized!");
-//        }
+        int k = 11;
+        for (int i = 0; i < 10; i++) {
+            Thread t = new Thread(new SynchronizedDemo()); //新建线程，每个线程都对count进程累加操作；
+            t.start();
+        }
+        try {
+            Thread.sleep(500);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        System.out.println(count);
+    }
+
+    @Override
+    public void run(){
+        synchronized (SynchronizedDemo.class){  //这个锁是类锁，每个类只有这一个锁
+            for (int i = 0; i < 1000000; i++) {
+                count++;
+            }
+        }
     }
 
 }
