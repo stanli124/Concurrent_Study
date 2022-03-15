8cbf00dbe4345793afcefe15eec40c2f350c3c49
stanli124 <1353276354@qq.com>
2022-03-15 11:12:14 +0800

    线程池创建测试

--- src/threadPool/threadPoolTest.java
+++ src/threadPool/threadPoolTest.java
@@ -9,7 +9,7 @@ public class threadPoolTest {
 
     private static final int CORE_POOL_SIZE = 5;
     private static final int MAX_POOL_SIZE = 10;
-    private static final int QUEUE_CAPACITY = 100;
+    private static final int QUEUE_CAPACITY = 10;
     private static final Long KEEP_ALIVE_TIME = 1L;
 
     public static void main(String[] args) {
@@ -31,7 +31,7 @@ public class threadPoolTest {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("CurrentThread name:" + Thread.currentThread().getName() + "date：" + Instant.now());
+                System.out.println("CurrentThread name:" + Thread.currentThread().getName() + "  date：" + Instant.now());
             });
         }
         //终止线程池
