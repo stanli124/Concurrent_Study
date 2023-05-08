476c26afe4d880930c69e2837c4b57537bd42917
stanli124 <1353276354@qq.com>
2023-05-08 21:33:04 +0800

    测试线程的状态-2

--- src/ThreadState.java
+++ src/ThreadState.java
@@ -1,12 +1,14 @@
 public class ThreadState {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             System.out.println();
             System.out.println(Thread.currentThread());
         }, "test");
         System.out.println(thread.getState() + thread.getName());
         thread.start();
+        System.out.println(thread.getState() + thread.getName());
+        thread.join(1000);
         System.out.println(thread.getState() + thread.getName());
     }
 
