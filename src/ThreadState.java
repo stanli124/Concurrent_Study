ab90d70d071b4d0a7ffb3c8991346583947f12e9
stanli124 <1353276354@qq.com>
2023-05-09 16:55:17 +0800

    测试线程的状态-线程阻塞与中断

--- src/ThreadState.java
+++ src/ThreadState.java
@@ -3,13 +3,25 @@ public class ThreadState {
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             System.out.println();
-            System.out.println(Thread.currentThread());
-        }, "test");
-        System.out.println(thread.getState() + thread.getName());
-        thread.start();
-        System.out.println(thread.getState() + thread.getName());
-        thread.join(1000);
-        System.out.println(thread.getState() + thread.getName());
+            System.out.println(Thread.currentThread().getState()+Thread.currentThread().getName());
+            try {
+                Thread.currentThread().join(1000); //主线程休眠后执行这段代码，等待1.5秒
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "11");
+        System.out.println(thread.getState() + thread.getName()); //刚刚创建线程，是new状态
+
+        thread.run();
+        System.out.println(thread.getState() + thread.getName()); //调用start后是可运行状态
+
+        Thread.sleep(500); //主线程休眠
+        System.out.println(thread.getState() + thread.getName()); //thread等待过程中，执行主线程，此时thread是timed waiting状态
+
+        //Thread.currentThread().interrupt(); 这里演示主线程执行sleep后，若再执行中断方法，会抛出一个异常；
+
+        Thread.sleep(1000); //主线程休眠1s等待thread执行完
+        System.out.println(thread.getState() + thread.getName()); //此时thread执行完，进入终止状态
     }
 
 }
